7b3c7508ce06cbc27e30779f32902a7223e218ce
Tianhao Shen <magnetic-world@outlook.com>
2024-02-28 23:10:29 +0800

    Update demo

--- demo/utils/const.py
+++ demo/utils/const.py
@@ -27,33 +27,8 @@ class_denial = """Class Denial:
 """
 
 GUARD_CODE = f"""
-import builtins
-
-_original_open = open
-
-def custom_open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None):
-    if 'w' in mode or 'a' in mode or 'x' in mode or '+' in mode:
-        raise PermissionError("Writing operation is not permitted due to safety reasons. Please do not try again!")
-    return _original_open(file, mode, buffering, encoding, errors, newline, closefd, opener)
-
-builtins.open = custom_open
-
-builtins.exit = {write_denial_function}
-builtins.quit = {write_denial_function}
-
-import sys
-
-blocked_modules = ['pathlib', 'glob', 'ctypes']
-
-for module in blocked_modules:
-    sys.modules[module] = PermissionError
-
 import os
 
-os.listdir = {read_denial_function}
-os.scandir = {read_denial_function}
-os.walk = {read_denial_function}
-os.stat = {read_denial_function}
 os.kill = {write_denial_function}
 os.system = {write_denial_function}
 os.putenv = {write_denial_function}
@@ -82,9 +57,6 @@ os.lchown = {write_denial_function}
 os.getcwd = {write_denial_function}
 os.chdir = {write_denial_function}
 os.popen = {write_denial_function}
-os.environ = {{}}
-os.getenv = {write_denial_function}
-builtins.open = {write_denial_function}
 
 import shutil
 
@@ -96,8 +68,6 @@ import subprocess
 
 subprocess.Popen = {write_denial_function}  # type: ignore
 
-__builtins__["help"] = {write_denial_function}
-
 import sys
 
 sys.modules["ipdb"] = {write_denial_function}
@@ -105,8 +75,6 @@ sys.modules["joblib"] = {write_denial_function}
 sys.modules["resource"] = {write_denial_function}
 sys.modules["psutil"] = {write_denial_function}
 sys.modules["tkinter"] = {write_denial_function}
-
-get_ipython().system = lambda *args, **kwargs: (_ for _ in ()).throw(PermissionError("Sorry, magic command is disabled due to safety reasons. Please do not try again!"))
 """
 
 CODE_INTERPRETER_SYSTEM_PROMPT = """You are an AI code interpreter.
